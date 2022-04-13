89ebfae463d68a6cff62db44a3eb111000439528
Akshay <akshaysivan99@gmail.com>
2022-04-13 11:47:03 +0530

    version 1

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	int a = 10;
+	int b = 20;
+	int c = a + b;
+	cout << a << " + " << b << " = " << c <<"\n";
+	int lst[5] = { 1,2,3,4,5 };
+	cout << "List items are : ";
+	for ( int i = 0; i < 5; i++)
+	{
+		cout << lst[i] << " ";
+
+	}
+}
+
